cebea6b9d572d3e66b1b8d29329dbe52a0926ed9
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-22 17:23:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(katz_deli)
+  #at the beginning of the day, the deli is empty
+  #so the queue should be represented by an empty array
+  katz_deli = []
+  if katz_deli == nil
+    puts "The line is currently empty."
+  end
+  
+  def take_a_number(katz_deli, element)
